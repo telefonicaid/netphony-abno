1e4c5744893d595aa65ac8e92fde70805e510d54
VictorUceda <victoruceda2@gmail.com>
2016-05-03 17:58:02 +0200

    modified files to new rawockect library location

--- src/main/java/es/tid/topology/topologymodule/ospfRead/ReadRawSocketServer.java
+++ src/main/java/es/tid/topology/topologymodule/ospfRead/ReadRawSocketServer.java
@@ -1,6 +1,6 @@
 package es.tid.topology.topologymodule.ospfRead;
 
-import static com.savarese.rocksaw.net.RawSocket.PF_INET;
+import static es.tid.rocksaw.net.RawSocket.PF_INET;
 
 import java.io.IOException;
 import java.net.Inet4Address;
@@ -8,7 +8,7 @@ import java.net.InetAddress;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.logging.Logger;
 
-import com.savarese.rocksaw.net.RawSocket;
+import es.tid.rocksaw.net.RawSocket;
 
 import es.tid.ospf.ospfv2.OSPFPacketTypes;
 import es.tid.ospf.ospfv2.OSPFv2HelloPacket;
